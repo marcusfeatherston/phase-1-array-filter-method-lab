a6bb33cefdf6820d45aa00592763d511a584c8a3
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-17 17:05:44 -0700

    created 3 filter method functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+  const pickDrivers = drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+  return pickDrivers;
+};
+
+console.log(
+  findMatching(
+    ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"],
+    "Bobby"
+  )
+);
+
+const fuzzyMatch = (drivers, name) => {
+  const pickDrivers = drivers.filter((driver) => driver.startsWith(name));
+  return pickDrivers;
+};
+
+console.log(
+  fuzzyMatch(["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bob"], "Bo")
+);
+
+const matchName = (drivers, name) => {
+  const pickDrivers = drivers.filter((driver) => driver.name === name);
+  return pickDrivers;
+};
